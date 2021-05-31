8fa2f878272c540f77006df9b7d97e3593a6dc81
Kiruse <26940742+Kiruse@users.noreply.github.com>
2021-05-31 02:12:22 +0200

    v1.8: add Optional meta type

--- src/Types.jl
+++ src/Types.jl
@@ -15,6 +15,41 @@ Base.getindex(mutable::Mutable) = mutable.value
 Base.setindex!(mutable::Mutable{T}, value::T) where T = mutable.value = value
 Base.:(==)(lhs::Mutable{T}, rhs::Mutable{T}) where T = lhs.value == rhs.value
 
+export Unknown, unknown
+struct Unknown end
+const unknown = Unknown()
+
+export Optional
+mutable struct Optional{S, T}
+    value::Union{T, Unknown}
+    Optional{S, T}(value) where {S, T} = new(convert(T, value))
+    Optional{S, T}(::Unknown) where {S, T} = new(unknown)
+    Optional{S, T}(value::T) where {S, T} = new(value)
+end
+Optional{S, T}() where {S, T} = Optional{S, T}(unknown)
+Optional{S}(value::T) where {S, T} = Optional{S, T}(value)
+Optional{S}(::Unknown) where S = Optional{S, Any}(unknown)
+Optional{S}() where S = Optional{S}(unknown)
+Optional(S::Symbol, value::T) where T = Optional{S, T}(value)
+Optional(value::T) where T = Optional{:generic, T}(value)
+Optional(::Unknown) = Optional{:generic, Any}(unknown)
+Optional() = Optional(unknown)
+
+Base.getindex(opt::Optional) = opt.value
+Base.setindex!(opt::Optional, value) = opt.value = value
+
+Base.convert(::Type{Optional{S}}, value) where S = Optional{S}(value)
+Base.convert(::Type{Optional{S}}, opt::Optional) where S = (println("convert $(opt.value)"); Optional{S}(opt.value))
+Base.convert(::Type{Optional{S, T}}, value) where {S, T} = Optional{S, T}(value)
+Base.convert(::Type{Optional{S, T}}, opt::Optional) where {S, T} = Optional{S, T}(opt.value)
+
+Base.convert(::Type{Optional{S} where S}, value) = Optional{:generic}(value)
+Base.convert(::Type{Optional{S, T} where S}, value) where T = Optional{:generic, T}(value)
+Base.convert(::Type{Optional{S} where S}, opt::Optional) = opt
+Base.convert(::Type{Optional{S1, T} where S1}, opt::Optional{S2, T} where S2) where T = opt
+Base.convert(::Type{Optional{S1, T} where S1}, opt::Optional{S2}) where {S2, T} = Optional{S2, T}(opt.value)
+
+
 export Dirty, markdirty!, isdirty
 mutable struct Dirty{T}
     value::T
--- test/Test.Types.jl
+++ test/Test.Types.jl
@@ -24,6 +24,55 @@ import ..Helpers: Immutable
         end
     end
     
+    @testset "Optional" begin
+        @testset "explicit" begin
+            struct WithExplicitOptional{T}
+                opt::Optional{:explicit, T}
+            end
+            WithExplicitOptional{T}() where T = WithExplicitOptional{T}(unknown)
+            
+            @test WithExplicitOptional{Real}().opt[] === unknown
+            @test WithExplicitOptional{Real}(Optional()).opt[] === unknown
+            @test WithExplicitOptional{Integer}(24).opt[] == 24
+            @test WithExplicitOptional{Integer}(42.).opt[] == 42
+            @test WithExplicitOptional{Integer}(Optional(42.)).opt[] == 42
+            let obj = WithExplicitOptional{Real}(24)
+                obj.opt[] = 69.69
+                @test obj.opt[] == 69.69
+            end
+            let obj = WithExplicitOptional{Integer}()
+                obj.opt[] = 420
+                @test obj.opt[] == 420
+            end
+        end
+        
+        @testset "generic" begin
+            struct WithGenericOptional
+                opt::Optional{:generic}
+            end
+            WithGenericOptional() = WithGenericOptional(unknown)
+            
+            @test WithGenericOptional().opt[] === unknown
+            @test WithGenericOptional(24).opt[] == 24
+            @test WithGenericOptional(Optional(24)).opt[] == 24
+            let obj = WithGenericOptional()
+                obj.opt[] = 24
+                @test obj.opt[] == 24
+            end
+        end
+        
+        @testset "symbolless" begin
+            struct WithSymbollessOptional{T}
+                opt::Optional{S, T} where S
+            end
+            WithSymbollessOptional{T}() where T = WithSymbollessOptional{T}(unknown)
+            
+            @test WithSymbollessOptional{Real}().opt[] === unknown
+            @test WithSymbollessOptional{Real}(Optional(42)).opt[] == 42
+            @test WithSymbollessOptional{Integer}(Optional(42.)).opt[] == 42
+        end
+    end
+    
     @testset "Dirty" begin
         let dirty = Dirty(42)
             @test !isdirty(dirty)
